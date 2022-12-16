07b7dcdf39dc5d002d867ddf2fc48fa4e4d62f3b
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-12-16 14:07:54 +0100

    Add  template for card content

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,15 +3,23 @@ import './card.css';
 
 const Card = () => {
 	return (
-		<div className='card'>
-			<article>
-				<h2>Taxi Driver was released in which year?</h2>
-				<ul>
-					<li>Film</li>
-					<li>Film and TV</li>
-				</ul>
-			</article>
-		</div>
+		<article className='card'>
+			<h2 className='question'>Taxi Driver was released in which year?</h2>
+			<ul className='answers'>
+				<li>
+					<button className='choice'>Answer 1</button>
+				</li>
+				<li>
+					<button className='choice'>Answer 2</button>
+				</li>
+				<li>
+					<button className='choice'>Answer 3</button>
+				</li>
+				<li>
+					<button className='choice'>Answer 4</button>
+				</li>
+			</ul>
+		</article>
 	);
 };
 
